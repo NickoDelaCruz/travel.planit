1491697fed461201f295c8ed7f7683666d01530d
Masen Matthews and Nicko Dela Cruz <nicko@>
2018-03-05 09:57:20 -0800

    add initial file structure

---
+++ app.rb
@@ -0,0 +1,16 @@
+require("sinatra")
+require("sinatra/reloader")
+require("sinatra/activerecord")
+also_reload("lib/**/*.rb")
+require("./lib/________")
+require("./lib/________")
+require("./lib/________")
+require("shoulda-matchers")
+require("pry")
+require("pg")
+
+DB = PG.connect({:dbname => "travel_planit_test"})
+
+get('/') do
+  erb(:index)
+end
