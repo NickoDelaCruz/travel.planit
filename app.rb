2e8a4d2cbf00e9c867758435268f60db6835a3c5
Emily Watkins and Hannah Lee <emily@>
2018-03-07 10:33:34 -0800

    add routing to user page.

--- app.rb
+++ app.rb
@@ -8,26 +8,32 @@ get('/') do
   erb(:index)
 end
 
-post('/') do
-  @user = params['name']
+post('/users') do
+  name = params['name']
+  @user = User.create(:name => name)
+  redirect("/users/".concat(@user.id.to_s))
+end
+
+get('/users/:id') do
+  @user = User.find(params['id'].to_i)
   erb(:user)
 end
 
-get('/user/:id') do
-  @name = Name.find(params['id'].to_i)
-  destination = params('destination')
-  date = params('date')
-  new_trip = Trip.find_or_create_by(:destination => destination, :date => date)
-  erb(:trip)
-end
-
-post('/user/:id/trip') do
-  @name = Name.find(params['id'].to_i)
-  description = params('description')
-  user_id = params('user_id')
-  new_activity = Activity.find_or_create_by(:description => description, :user_id => user_id)
-  erb(:trip)
-end
+# get('/user/:id') do
+#   @name = Name.find(params['id'].to_i)
+#   destination = params('destination')
+#   date = params('date')
+#   new_trip = Trip.find_or_create_by(:destination => destination, :date => date)
+#   erb(:trip)
+# end
+#
+# post('/user/:id/trip') do
+#   @name = Name.find(params['id'].to_i)
+#   description = params('description')
+#   user_id = params('user_id')
+#   new_activity = Activity.find_or_create_by(:description => description, :user_id => user_id)
+#   erb(:trip)
+# end
 
 get('/weather') do
   @current = {}
