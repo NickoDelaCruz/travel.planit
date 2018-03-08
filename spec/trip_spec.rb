55a86ef0b92f4bdd7c1b4b77b6ea4352bef8a56a
Emily Watkins and Hannah Lee <emily+hannah@>
2018-03-08 13:49:35 -0800

    tweak specs.

--- spec/trip_spec.rb
+++ spec/trip_spec.rb
@@ -2,15 +2,13 @@ require('spec_helper')
 
 describe(Trip) do
 
-  describe('#get_weather') do
-    it('will return API response') do
-      new_trip = Trip.new(:latitude => 45.5231, :longitude => -122.6765)
-      # new_weather = new_trip.get_weather(45.5231, -122.6765)
-      expect(new_trip.get_weather).to respond_with_content_type(:json)
-    end
-  end
   # describe('#get_weather') do
-  #   it { is_expected.to respond_with_content_type(:json) }
+  #   it('will return API response') do
+  #     new_trip = Trip.new(:latitude => 45.5231, :longitude => -122.6765)
+  #     new_weather = new_trip.get_weather(45.5231, -122.6765)
+  #     binding.pry
+  #     expect(new_weather.response).to have_http_status(200)
+  #   end
   # end
 
 end
