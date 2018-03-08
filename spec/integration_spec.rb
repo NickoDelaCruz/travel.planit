050e83cd6c679646d50a257213be894466d358f5
Emily Watkins and Hannah Lee <hannah@>
2018-03-08 10:32:49 -0800

    add activity creation integration spec.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -21,3 +21,16 @@ describe 'the trip creation path', {:type => :feature} do
     expect(page).to have_content('portland')
   end
 end
+
+describe 'the activity creation path', {:type => :feature} do
+  it 'takes the user to the trip page where they can add an activity' do
+    test_user = User.create({:name => 'emily'})
+    user_id = test_user.id.to_i
+    test_trip = Trip.create({:destination => 'portland', :latitude => 45.5231, :longitude => -122.6765})
+    trip_id = test_trip.id.to_i
+    visit "/trips/#{trip_id}"
+    fill_in('description', :with => 'eat')
+    click_button('Add Activity')
+    expect(page).to have_content('eat')
+  end
+end
