66178254555ad9f5540977d979008ee9f36a70a8
Emily Watkins and Hannah Lee <hannah@>
2018-03-08 10:39:44 -0800

    add integration spec for trips page.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -34,3 +34,13 @@ describe 'the activity creation path', {:type => :feature} do
     expect(page).to have_content('eat')
   end
 end
+
+describe 'the trips page', {:type => :feature} do
+  it 'takes the user to their trips page where they can see all saved trips' do
+    test_user = User.create({:name => 'emily'})
+    user_id = test_user.id.to_i
+    test_trip = Trip.create({:destination => 'portland', :latitude => 45.5231, :longitude => -122.6765, :user_id => user_id})
+    visit "/users/#{user_id}/trips"
+    expect(page).to have_content('portland')
+  end
+end
